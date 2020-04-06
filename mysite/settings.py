3ef457458c1e78ef7c043804e5e439e2f9b910c9
midascm <midascmmaster@gmail.com>
2020-04-06 22:55:48 +0900

    commit_ver_03

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'news.apps.NewsConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'news',
 ]
 
 MIDDLEWARE = [
